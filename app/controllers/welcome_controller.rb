dc929b0205448e32a00a3860141e9be07445aad8
Agnivesh <agnivesh@betatestsolutions.com>
2017-09-04 17:54:54 +0530

    oauth setup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  #Actions
+  before_action :authenticate_user!
   def index
+
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ENV['GOOGLE_CLIENT_ID']    = '141991908011-dr5kuv04qhgng9mk9iootj7rkj8dfr2o.apps.googleusercontent.com'
+ENV['GOOGLE_CLIENT_SECRET']    = 'CLfjBspw2njw5H2j1m152fZO'
 # Initialize the Rails application.
 Rails.application.initialize!
