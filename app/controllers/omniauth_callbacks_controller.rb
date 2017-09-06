97e57198a4cb57425f1171cfbeaef62c84ca593c
Agnivesh <agnivesh@bts.betatest.in>
2017-09-06 19:33:40 +0530

    google oauth2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #binding.pry
   def all
-    #logger.debug request.env["omniauth.auth"]
+    #logger.debug "asd"
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "Signed in!"
